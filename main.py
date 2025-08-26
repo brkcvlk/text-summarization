25a315a414193dd19058f08e521d1a6208f1975f
Burak <burakcivelekk61@gmail.com>
2025-08-26 21:44:32 +0300

    Add .dockerignore
    - Update gitignore
    - Replace the model with a smaller one
    - Update requirements.txt

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mlflow
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("text_summarization")
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn", max_length=130, min_length=30, do_sample=False)
+summarizer = pipeline("summarization", model="t5-base", max_length=130, min_length=30, do_sample=False)
 
 llm = HuggingFacePipeline(pipeline=summarizer)
 
